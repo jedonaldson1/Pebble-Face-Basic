2d148cc2b7160ddb943aba982cc87d265f68be72
Jordan Donaldson <jedonaldson1@users.noreply.github.com>
2016-09-15 10:29:49 -0500

    Date
    Fixed date printing and made date change automatically. Also changed background color to DarkCandyAppleRed

--- src/c/main.c
+++ src/c/main.c
@@ -21,14 +21,17 @@ static void update_time()
   //Get a tm structure
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-  
   //Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? 
            "%H:%M" : "%I:%M", tick_time);
-  
   //Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+  
+  //Create a date buffer
+  static char date_buffer[16];
+  strftime(date_buffer, sizeof(date_buffer), "%a %b %e", tick_time);
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 //Function to handle time ticks
@@ -59,13 +62,13 @@ static void main_window_load(Window *window)
   //Create TextLayer with specific bounds (date)
   s_date_layer = text_layer_create(
     GRect(0, 105, bounds.size.w, 50));
-  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOUCISAN_25));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOUCISAN_23));
   //Improve layout
   text_layer_set_background_color(s_date_layer,GColorClear);
   text_layer_set_text_color(s_date_layer,GColorWhite);
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  text_layer_set_text(s_date_layer, "Wed Sep 14");
+  //text_layer_set_text(s_date_layer, "Wed Sep 14");
   //Add layer as child to Window
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
@@ -100,7 +103,7 @@ static void init()
   update_time();
   
   //Sets background color of window
-  window_set_background_color(s_main_window,GColorOxfordBlue);
+  window_set_background_color(s_main_window,GColorDarkCandyAppleRed);
   
   //Register with TickTimer service
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
