98399b13ae98a8fc346d4a26c5cf0eb3db76619a
Jordan Donaldson <jedonaldson1@users.noreply.github.com>
2016-11-08 13:45:35 -0600

    Trying to figure out coordinates of GRect
    Can't figure out arguments and CP is being dumb

--- src/c/main.c
+++ src/c/main.c
@@ -120,7 +120,7 @@ static void main_window_load(Window *window)
   //Create the Bluetooth icon GBitmap
   s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BLUETOOTH_ICON);
   //Create the BitmapLayer to display the GBitmap
-  s_bt_icon_layer = bitmap_layer_create(GRect(59, 12, 30, 30));
+  s_bt_icon_layer = bitmap_layer_create(GRect(62, 12, 30, 30));
   bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
   //Show the correct state of the BT connection from the start
